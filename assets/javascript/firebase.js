3a7f92eb780fc78a606e95ab874bb148f6f40329
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-03-21 20:55:35 -0500

    Updated firebase.js to show login and changed index to add a spot to sign into firebase

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,59 +1,52 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCwhOakfPVFjGYSWZ9KwaM8EH9lqw5cY1A",
+    authDomain: "teamawesome-f39d7.firebaseapp.com",
+    databaseURL: "https://teamawesome-f39d7.firebaseio.com",
+    projectId: "teamawesome-f39d7",
+    storageBucket: "",
+    messagingSenderId: "539466789478"
+  };
+  firebase.initializeApp(config);
 
-        var config = {
-            apiKey: "AIzaSyAn3KaitMECF3eoP-jwVBPKxM2u1_MOWQM",
-            authDomain: "teamawesome-c4e37.firebaseapp.com",
-            databaseURL: "https://teamawesome-c4e37.firebaseio.com",
-            projectId: "teamawesome-c4e37",
-            storageBucket: "teamawesome-c4e37.appspot.com",
-            messagingSenderId: "426168129967"
-        };
-        firebase.initializeApp(config);
+var txtEmail = document.getElementById('txtEmail');
+var txtPassword = document.getElementById('txtPassword');
+var btnLogin = document.getElementById('btnLogin');
+var btnSignUp = document.getElementById('btnSignUp');
+var btnLogOut = document.getElementById('btnLogOut');
 
-var database = firebase.database();
-
-$("#submitUser").on("click", function(event) {
-    event.preventDefault();
-
-    //Grabs user input from the form on the page
-    var fName = $("#first_name").val().trim();
-    var lName = $("#last_name").val().trim();
-    var userPassword = $("#password").val().trim();
-    console.log(userPassword);
-    var userEmail = $("#email").val().trim();
-
-    //Create local "temporary" object for user data
-    var newUser = {
-        firstName: fName,
-        lastName: lName,
-        password: userPassword,
-        email: userEmail
-    };
-
-    //Upload user information to database
-    database.ref().push(newUser);
-    console.log(newUser);
-    
-    //Clears the text boxes
-    $("#first_name").val("");
-    $("#last_name").val("");
-    $("#password").val("");
-    $("#email").val("");
-
+//Add login event
+btnLogin.addEventListener('click', e => {
+  //   e.preventDefault();
+  //Get email and password
+  var email = txtEmail.value;
+  var pass = txtPassword.value;
+  var auth = firebase.auth();
+  console.log(auth)
+  //Sign in
+  var promise = auth.signInWithEmailAndPassword(email, pass);
+  promise.catch(e => console.log(e.message));
+  console.log('test')
 });
 
-//Create an event in database when user is added
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-    //Store data
-    var fName = childSnapshot.val().firstName;
-    var lName = childSnapshot.val().lastName;
-    var userPassword = childSnapshot.val().password;
-    var userEmail = childSnapshot.val().email;
+//Add SignUp event
+btnSignUp.addEventListener('click', e => {
+  //Get email and pass
+  //ToDo: Check 4 REAL email
+  // e.preventDefault();
+  var email = txtEmail.value;
+  var pass = txtPassword.value;
+  var auth = firebase.auth();
+  //Sign in
+  var promise = auth.createUserWithEmailAndPassword(email, pass);
+  promise.catch(e => console.log(e.message));
 });
 
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+//Add a realtime listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if (firebaseUser) {
+    console.log(firebaseUser);
+  } else {
+    console.log('not logged in');
+  }
+});
