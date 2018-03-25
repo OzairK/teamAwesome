dfab815709ca7862f7bcf8c96ea5bc01d881ad13
Kris <kris.bierma@gmail.com>
2018-03-24 19:13:10 -0500

    tabs working

--- assets/javascript/firebase-KEEP.js
+++ assets/javascript/firebase-KEEP.js
@@ -18,7 +18,6 @@ var btnLogOut = document.getElementById('btnLogOut');
 
 var uid; //get uid to create new node off root (1st level)
 var name; //get user's name
-// console.log(uid, name);
 
 //Add login event
 btnLogin.addEventListener('click', e => {
@@ -27,8 +26,7 @@ btnLogin.addEventListener('click', e => {
   var email = txtEmail.value;
   var pass = txtPassword.value;
   var auth = firebase.auth();
-  console.log(auth,email,pass);
-
+ 
   //Sign in
   var promise = auth.signInWithEmailAndPassword(email, pass);
   promise.catch(e => console.log(e.message)); //need to fix this so below code won't run if the promise.catch returns error message
@@ -39,8 +37,57 @@ btnLogin.addEventListener('click', e => {
     name=auth.currentUser.displayName;
     console.log(uid,name); //works
     $("#hiUser").text("Hi " + name);
+
+    // var d = firebase.database().ref(uid); //everything
+    // d.on("child_added", function(snapShot) {
+    //     console.log(uid);//current uid
+    //     console.log(snapShot.val());//child nodes of user
+    //     // trial(snapShot.val());
+    // });
+
+    //func below gets passed the uid and creates tabs
+    console.log("populating tabs from login");
+    populateTabs(uid);
   });
 });
+
+//makes the tabs; is called when user registers, logsin, or a tab is added
+function populateTabs(uid){
+    $(".tabs").empty();
+    // console.log(uid);
+    database.ref(uid).on("value", function(snapShot){
+        // console.log(snapShot);
+        var tabInfo=database.ref(uid).key;//uid
+        var chillins=snapShot.val();//children of current uid
+        // console.log(tabInfo);
+        // console.log(chillins);
+    
+        if (uid !== undefined){
+            var t1=chillins.tab1;//values of tab1
+            var t2=chillins.tab2;
+            var t3=chillins.tab3;
+            var tabInfo=database.ref(uid).key;
+            if (typeof t1 !== "boolean"){
+                var newTab=$("<li>").addClass("tab col s3");
+                var newA=$("<a id=tab1>").text(t1.tabName);
+                newTab.append(newA);
+                $(".tabs").append(newTab);
+            }
+            if (typeof t2 !== "boolean"){
+                var newTab=$("<li>").addClass("tab col s3");
+                var newA=$("<a id=tab2>").text(t2.tabName);
+                newTab.append(newA);
+                $(".tabs").append(newTab);
+            }
+            if (typeof t3 !== "boolean"){
+                var newTab=$("<li>").addClass("tab col s3");
+                var newA=$("<a id=tab3>").text(t3.tabName);
+                newTab.append(newA);
+                $(".tabs").append(newTab);
+            }
+        }
+    })
+};
 
 //Add SignUp event
 btnSignUp.addEventListener('click', e => {
@@ -50,8 +97,7 @@ btnSignUp.addEventListener('click', e => {
   var email = txtEmail.value;
   var pass = txtPassword.value;
   var name = txtName.value;//get name from form
-//   $("#hiUser").text("Hi " + name);
-  console.log(name);
+//   console.log(name);
   var auth = firebase.auth();
   //Sign in
   var promise = auth.createUserWithEmailAndPassword(email, pass)
@@ -59,29 +105,28 @@ btnSignUp.addEventListener('click', e => {
   //if error message above, stop code below!!!!
   promise.then(function(){
     var user = firebase.auth().currentUser;
-    console.log(user,uid);
-    name=user.uid;
     if (user != null){
-        console.log(user.uid);
         uid=user.uid;
+
         database.ref(uid).set({
-            name:name, //maybe delete later
+            name:name, //only here for reference in console
             tab1:false,
             tab2:false,
             tab3:false
         });
+
         user.updateProfile({
             displayName:name
         }).then(function(){
-            name=user.displayName;
+            // console.log(name, user.displayName);
+            $("#hiUser").text("Hi " + name);
         }, function(error){
             console.log("error");
         })
-        console.log(uid, name, user.displayName);
-        //user.displayName=null; why???
+        // console.log(uid, name, user.displayName); delete
     }; 
+    // console.log(name, user.displayName); delete
   });
-  $("#hiUser").text("Hi " + name);
   $("#loginForm")[0].reset();
 });
 
@@ -92,93 +137,50 @@ $("#btnSignOut").on("click", function(event){
         uid=undefined;
         name=undefined;
         console.log(uid,name);
+        window.location.reload();   
     }).catch(function(error){
         console.log("Error in signing out");
     });
     $("#loginForm")[0].reset();
 });
 
-//Add a realtime listener -- this works!!
+//A realtime listener for change in log in/out
 firebase.auth().onAuthStateChanged(function(user) {
   if (user!=null) {
-    uid=user.uid;
-    name=user.displayName;
-    console.log(uid, name);
-    $("#hiUser").text("Hi " + name);
-    //needs to detect user sign in and update page
-    
-
-
-} else {
-    console.log('not logged in');
-  }
+        uid=user.uid;
+        name=user.displayName;
+        // console.log("auth state change: "+uid, name);
+        console.log("populating tabs from auth state change");
+        // populateTabs(uid);
+        $("#hiUser").text("Hi " + name);
+    } 
+    else {console.log('not logged in');}
 });
 
 var database = firebase.database();
 
-//do we need this?????
-$("#submitUser").on("click", function(event) {
-    event.preventDefault();
-
-    //Grabs user input from the form on the page
-    var fName = $("#first_name").val().trim();
-    var lName = $("#last_name").val().trim();
-    var userPassword = $("#password").val().trim();
-    console.log(userPassword);
-    var userEmail = $("#email").val().trim();
-
-    //Create local "temporary" object for user data
-    var newUser = {
-        firstName: fName,
-        lastName: lName,
-        password: userPassword,
-        email: userEmail
-    };
-
-    //Upload user information to database
-    database.ref().push(newUser);
-    console.log(newUser);
-    
-    //Clears the text boxes
-    $("#first_name").val("");
-    $("#last_name").val("");
-    $("#password").val("");
-    $("#email").val("");
-
-});
-
-//do we need this???????
-//Create an event in database when user is added
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {   
-    var fName = childSnapshot.val().firstName;
-    var lName = childSnapshot.val().lastName;
-    var userPassword = childSnapshot.val().password;
-    var userEmail = childSnapshot.val().email;
-});
-
 $("#addTab").on("click", function(event){
     //to do:when add tab, add delete button to modal
     //to do:if delete btn clicked, delete fb info
-    // console.log(uid);
     uid=firebase.auth().currentUser.uid; 
     var tabName=$("#tabName").val().trim();
     var tabStreet=$("#tabStreet").val().trim();
     var tabCity=$("#tabCity").val().trim();
     var tabZip=$("#tabZip").val().trim();
-    console.log(tabName,tabStreet,tabCity,tabZip);
-    console.log(uid);//uid
 
     //make sure all info is included
     if(tabStreet!=""&&tabStreet!=""&&tabCity!=""&&tabZip!=""){
-        // cycle through all tabs
+
+        // if all info is included, cycle through all tabs
         label: for (var i=1; i<4; i++){
             var tabInfo=database.ref(uid).child("tab"+i).key;
             var tabValue;
+            var popTab=false;
             database.ref(uid).child("tab"+i).once("value", function(snap){
                 tabValue=snap.val();
-                console.log(tabValue);
             });
-            //if there's an empty (false) tab, push the data
+
+            //if there's an empty (false) tab, push the data and break the label loop above
             if (tabValue === false){
                 database.ref(uid).child(tabInfo).update({
                     tabName:capUpper(tabName),
@@ -186,22 +188,31 @@ $("#addTab").on("click", function(event){
                     tabCity:capUpper(tabCity),
                     tabZip:tabZip,
                 });
-                console.log(tabName);
+
+                popTab=true; //set flag to true so no error msg
+                console.log("populating tabs from add tab");
                 break label;
-            } 
-            //to do:else modal below with "You've reached the max tabs. Delete one first before adding another."
-            // else {console.log("You've reached the max tabs. Delete one first before adding another.")}   
+            }
         }
+
+            //if no tabs populated, max is reached, alert user
+            if (popTab===false){
+                alert("You've reached the max number of tabs. Delete one first before adding another.");
+            }
     }
+
+    //if missing tabName, address, city, or zip ->give error message
     else {
         console.log("error message - need more info");
     };
-    $("#mapTabs")[0].reset();
 
+    //reset the form
+    $("#mapTabs")[0].reset();
 });
 
-//populates tabs from firebase info
-database.ref(uid).on("value", function(snapShot) {
+ //populates tabs from firebase info
+ database.ref(uid).on("value", function(snapShot) {
+    console.log("populating tabs from listener")
     var tabInfo=database.ref(uid).key;//uid
     var chillins=snapShot.child(uid).val();//children of uid
     // console.log(t1, t2, t3); //keep this
